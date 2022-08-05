a3c1d1ccbdfcdf53a4f42e4d8b208228b40397b6
JakeFernandes98 <50297393+JakeFernandes98@users.noreply.github.com>
2022-08-05 11:10:44 +0100

    Commenting

--- example.ts
+++ example.ts
@@ -1,14 +1,18 @@
-const callUrl = 'https://dclapi.azurewebsites.net/api/'
+const callUrl = 'https://dclapi.azurewebsites.net/api/' //API URL
 let playerInfo;
-let parcelId = 1
-const apiEventManager = new EventManager()
+let parcelId = 1 //Hardcoded parcel Id
+const apiEventManager = new EventManager() //Initialising the Event Manager
 
+
+//Fetching user data may time some time, so it is better to fetch it as soon 
+//as the player enters the scene and then reuse the information while they remain on the scene
 function initAPI(){
     executeTask(async () => {
         playerInfo = await getUserData()
     })
 }
 
+// Defining an Event, where the experienceId is being passed as an argument
 @EventConstructor()
 export class EnterExperience {
   experienceId: number;
@@ -17,16 +21,20 @@ export class EnterExperience {
   }
 }
 
+// Defining the listener for the event, here we will make the API request
 apiEventManager.addListener(EnterExperience, null, ({experienceId}) => {
   if(!freeze){
     let json: JSON
+    
+    // generate a timestamp
     let timestamp = new Date()
     
+    // pull specific user data
     let isUserGuest = !(playerInfo.hasConnectedWeb3)
     let userId = isUserGuest ? playerInfo.userId : playerInfo.publicKey
     let userName = playerInfo.displayName
 
-  
+    // make API call
     executeTask(async () => {
         try {
             let response = await fetch(callUrl+"experience", {
@@ -51,15 +59,23 @@ apiEventManager.addListener(EnterExperience, null, ({experienceId}) => {
   }
 })
 
+// Define an entity for the trigger
 const floor = new Entity()
+// Position it in the middle of the experience
 floor.addComponent(new Transform({
   position: new Vector3(8,4,8)
 }))
 floor.addComponent(
+    // Add the trigger component
     new utils.TriggerComponent(
+        // Add the appropriate trigger shape with the appropriate sizing
       new utils.TriggerBoxShape(new Vector3(16, 4, 16), Vector3.Zero()),
           {
+          // When the user enters this trigger box, the event will fire
           onCameraEnter: () => {
             apiEventManager.fireEvent(new EnterExperience(1))        
+          },
+          // Can also make another call when the user exits
+          onCameraExit: () => {
           })
 )
